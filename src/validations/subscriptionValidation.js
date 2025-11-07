4d73992a1afc56973f9d5f2080d7c9d1d5a68278
Vladyslav <vladislav.putintsev@gmail.com>
2025-11-08 00:50:09 +0200

    e

--- src/validations/subscriptionValidation.js
+++
@@ -1,7 +0,0 @@
-import { celebrate, Joi, Segments } from 'celebrate';
-
-export const validateSubscribe = celebrate({
-  [Segments.BODY]: Joi.object({
-    email: Joi.string().email().required(),
-  })
-});
